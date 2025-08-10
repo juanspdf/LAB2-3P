ac2328b6c0b2be231542c2de2e29f066151bdd37
juanspdf <jdpasquel1@espe.edu.ec>
2025-08-10 16:57:49 -0500

    test: fix failing test

--- math.test.js
+++ math.test.js
@@ -1,24 +1,30 @@
-
+// math.test.js
 const math = require('./math.js');
 
 test('factorial de 3 debe ser 6', () => {
   expect(math.factorial(3)).toBe(6);
 });
+
 test('fibonacci de 4 debe ser 3', () => {
   expect(math.fibonacci(4)).toBe(3);
 });
 
 test('factorial de -1 debe lanzar error', () => {
-  expect(() => math.factorial(-1)).toThrow('n debería ser un entero no negativo');
+  expect(() => math.factorial(-1))
+    .toThrow('n debería ser un entero no negativo');
 });
 
 test('fibonacci de -1 debe lanzar error', () => {
-  expect(() => math.fibonacci(-1)).toThrow('n debería ser un entero no negativo');
+  expect(() => math.fibonacci(-1))
+    .toThrow('n debería ser un entero no negativo');
 });
+
 test('factorial de 0 debe ser 1', () => {
   expect(math.factorial(0)).toBe(1);
 });
 
-//Forzar la falla del test
+// Forzar la falla del test (intencional)
+test('fib(5) = 5', () => {
+  expect(math.fibonacci(5)).toBe(5);
+});
 
-test('fib(5) = 7 (intencionalmente mal)', () => expect(fibonacci(5)).toBe(7));
